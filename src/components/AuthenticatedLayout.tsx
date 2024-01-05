547ede6019d25e1257f01ad1839ef013da526af2
Cabral <Cabralzinho@users.noreply.github.com>
2024-01-05 14:01:51 -0300

    ultimo update

--- src/components/AuthenticatedLayout.tsx
+++ src/components/AuthenticatedLayout.tsx
@@ -1,3 +1,20 @@
-export const AuthenticatedLayout = ({ children }: { children: JSX.Element }) => {
-  return children
+import { auth } from "@/firebase/firebase";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AuthenticatedLayout = ({
+  children,
+}: {
+  children: JSX.Element;
+}) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.currentUser) {
+      navigate("/login");
+      return;
+    }
+  }, [auth]);
+
+  return children;
 };
